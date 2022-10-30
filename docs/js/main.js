aaa265630cb125aede24fba96d45f20b30114070
saitamasaitama <s-yoshihara@saitama-tec.co.jp>
2022-10-30 20:09:17 +0900

    お

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,6 +1,7 @@
 console.log(1);
 for(let i=0;i<10;i++){
-  const a= $("<span>OK</span>");
+  const a= $("<span>□</span>");
   $("#main").append(a);
 }
+$("#main").on("click",function(){alert(66);});
 alert(50);
