9922e6e906122afc3dc366708747be9154d8b191
saitamasaitama <s-yoshihara@saitama-tec.co.jp>
2022-10-31 08:22:24 +0900

    jyanken first

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,7 +1,20 @@
 console.log(1);
-for(let i=0;i<10;i++){
-  const a= $("<span>□</span>");
-  $("#main").append(a);
+function Life(){
+  return $("<span>🟩</span>");
 }
+const A=$("#A");
+const B=$("#B");
+//体力初期化
+for(let i=0;i<6;i++){
+  $("#A").append(Life());
+  $("#B").append(Life());
+}
+
+function tick(){
+  if(A.children().length < 12) A.append(Life());
+  if(B.children().length < 12) B.append(Life());
+}
+
+setInterval(tick,1000);
 $("#main").on("click",function(){alert(66);});
-alert(50);
+//alert(50);
