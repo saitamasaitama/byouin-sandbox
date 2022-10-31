43a1e3cb0978dfd7d723444c613db0d4d46a4d4d
saitamasaitama <s-yoshihara@saitama-tec.co.jp>
2022-10-31 20:03:21 +0900

    g

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,10 +1,16 @@
 console.log(1);
 function Life(x ,y){
-  const result= $("<span>🟩</span>");
+  const result= $("<span>■</span>");
   result.addClass("dot");
   result.data("x",x)
-  result.css("left",x+"em");
-  result.css("top",y+"em");
+  result.data("y",y)
+  result.data("current",true);
+  result.data("next",true);
+  result.css("left",x*1.1+"em");
+  result.css("top",y*1.1+"em");
+  result.addClass("life");
+  
+  result.on("click",function(){alert(x+":"+y);});
  // result.addClass("x-5");
   return result;
 }
@@ -15,7 +21,7 @@ const A=$("#A");
 const B=$("#B");
 const Main=$("#Main");
 
-const table=[];
+const Table=[];
 for(let y=0;y<16;y++){
   const line=[];
   for(let x=0;x<16;x++){
@@ -24,21 +30,56 @@ for(let y=0;y<16;y++){
     Main.append(life)
     line.push(life);
   }
-  table.push(line);
+  Table.push(line);
 }
 // table done
-for(let i=0;i<6;i++){
-//  A.append(Life());
-//  B.append(Life());
-}
 
 function tick(){
-  const life=Life(8,4);
-  life.addClass("dot");
- // life.addClass("x-9");
-  if(A.children().length < 12) A.append(life);
- // if(B.children().length < 12) B.append(Life());
+
+  for(let y=0;y<16;y++)
+  for(let x=0;x<16;x++)
+  Table[x][y].data("next",false);
+  
+  updateLives();
+}
+function liveCheck(x,y){
+  let count=0;
+  if(liveCheckC(x-1,y-1))count++;
+  if(liveCheckC(x,y-1))count++;
+  if(liveCheckC(x+1,y-1))count++;
+  if(liveCheckC(x-1,y))count++;
+  if(liveCheckC(x,y))count++;
+  if(liveCheckC(x+1,y))count++;
+  if(liveCheckC(x-1,y+1))count++;
+  if(liveCheckC(x,y+1))count++;
+  if(liveCheckC(x+1,y+1))count++;
+    
+   
 }
 
+function liveCheckC(x,y){
+  if(x<0)return false;
+  if(16<=x)return false;
+  if(y<0)return false;
+  if(16<=y)return false;
+  return Table[x][y].data("current");
+}
+
+function updateLives(){
+  for(let y=0;y<16;y++){
+    for(let x=0;x<16;x++){
+      const e= Table[x][y];
+      e.removeClass("dead");
+      e.removeClass("life");
+      if(e.data("next")){
+        e.addClass("life");
+      }else{
+        e.addClass("dead")
+      }
+    }
+  }
+ 
+  
+}
 setInterval(tick,1000);
 //alert(50);
